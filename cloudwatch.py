3df810dc8f879f96ffd982dd272eb436d2715db0
Hünkar Özcan <hunkarozcan@gmail.com>
2015-07-14 22:31:35 +0300

    Cloudwatch Support

---
+++ cloudwatch.py
@@ -0,0 +1,22 @@
+﻿import boto.ec2.cloudwatch as cw
+import app_settings
+import logging
+class Cloudwatch(object):
+    """description of class"""
+
+    
+    
+    def __init__(self):
+        
+        self.conf=app_settings.Config()
+        print(self.conf.region)
+        self.conn = cw.connect_to_region(self.conf.region)
+        print(self.conn)
+
+    def send_metric(self, name, unit, value):
+        try:
+            self.conn.put_metric_data(namespace=self.conf.aws_cw_namespace, name=name, unit=unit, value=value)
+        except:
+            logging.error("Can't send metric to CloudWatch");
+        
+
