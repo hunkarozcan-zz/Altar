33f7a24890d4755fdf4f05f975c099340ac98f24
Hünkar Özcan <hunkarozcan@gmail.com>
2015-07-15 16:16:09 +0300

    Check queue connection

--- altar.py
+++ altar.py
@@ -18,26 +18,25 @@ while True:
 
     try:
         messages=qu.getMessage(1)
+        for msg in messages:
+            img=image.image(msg.get_body())
+            if img.downloadFromS3():
+                try:
+                    img.optimize()
+                    if img.check_results():
+                        logging.info("Optimization is a success. Uploading...")
+                        img.uploadToS3()
+                        cw.send_metric(name="Optimized Count",unit="Count",value="1.0")
+                    else:
+                        logging.warning("No need to Upload")
+
+                    msg.delete()
+                except: 
+                    #TODO: Do something meaningful with this
+                    logging.error("Error")
+                    cw.send_metric(name="Error Count",unit="Count",value="1.0")
+                    raise
     except:
         logging.error("Couldn't get messages")
-
-    for msg in messages:
-        img=image.image(msg.get_body())
-        if img.downloadFromS3():
-            try:
-                img.optimize()
-                if img.check_results():
-                    logging.info("Optimization is a success. Uploading...")
-                    img.uploadToS3()
-                    cw.send_metric(name="Optimized Count",unit="Count",value="1.0")
-                else:
-                    logging.warning("No need to Upload")
-
-                msg.delete()
-            except: 
-                #TODO: Do something meaningful with this
-                logging.error("Error")
-                cw.send_metric(name="Error Count",unit="Count",value="1.0")
-                raise
 
 logging.info('Altar Ended')
