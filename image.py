52f2392c1490be8b8fab8073626d339bb397292f
Hünkar Özcan <hunkarozcan@gmail.com>
2015-07-23 18:25:33 +0300

    PngOptimizerCL parameter fix

--- image.py
+++ image.py
@@ -125,7 +125,7 @@ class image():
         if img_type=='png':
 	        # It's a png, optipng is the way!
             # f=subprocess.check_output(['optipng', self.tf.name, '-o 2'])
-            f=subprocess.check_output(['PngOptimizerCL', self.tf.name])
+            f=subprocess.check_output(['PngOptimizerCL', '--file:',self.tf.name])
         elif img_type=='jpeg':
 	        # "jpg is the file, use jpegoptim you must" -Yoda
             f=subprocess.check_output(['jpegoptim',self.tf.name,'-v'])
